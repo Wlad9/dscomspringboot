0dd4377d316d3258e9aaec773b23380f43f933e8
wlad9 <zav8b9@gmail.com>
2023-07-12 16:45:48 -0300

    Implementado o modelo de domínio

--- src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -1,9 +1,17 @@
 package com.devsuperior.dslist.entities;
 
-import java.util.Objects;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+import java.util.Objects;
+@Embeddable
 public class BelongingPK {
+    @ManyToOne
+    @JoinColumn(name = "game_id")
     private Game game;
+    @ManyToOne
+    @JoinColumn(name = "gameList_id")
     private GameList list;
 
     public BelongingPK() {
